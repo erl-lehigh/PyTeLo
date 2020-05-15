6b30cbb21576672f1026eedf9669e989412a51bd
Cristian-Ioan Vasile <cvasile@mit.edu>
2020-05-15 11:35:29 -0400

    Added robustness bounds option via the range dictionary parameter.

--- stl/stl2milp.py
+++ stl/stl2milp.py
@@ -16,9 +16,12 @@ class stl2milp(object):
                  robust=False):
         self.formula = formula
 
+        self.M = 1000
         self.ranges = ranges
         if ranges is None:
             self.ranges = {v: (0, 10) for v in self.formula.variables()}
+        if robust and 'rho' not in self.ranges:
+            self.ranges['rho'] = (-grb.GRB.INFINITY, self.M - 1)
 
         self.vtypes = vtypes
         if vtypes is None:
@@ -28,12 +31,12 @@ class stl2milp(object):
         if model is None:
             self.model = grb.Model('STL formula: {}'.format(formula))
 
-        self.M = 1000
         self.variables = dict()
 
         if robust:
-            self.rho = self.model.addVar(vtype=grb.GRB.CONTINUOUS, name='rho',
-                               lb=-grb.GRB.INFINITY,ub=grb.GRB.INFINITY, obj=-1)
+            rho_min, rho_max = self.ranges['rho']
+            self.rho = self.model.addVar(vtype=self.vtype['rho'], name='rho',
+                                         lb=rho_min, ub=rho_max, obj=-1)
         else:
             self.rho = 0
 
