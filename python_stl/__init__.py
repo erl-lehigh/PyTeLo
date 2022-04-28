c687fd23fa03552ddd558210c2da0a569ed5ffa7
Makai Mann <makai.mann@ll.mit.edu>
2022-04-28 15:03:22 -0400

    Add original stl2milp code -- only imported if gurobipy available

--- python_stl/__init__.py
+++ python_stl/__init__.py
@@ -2,4 +2,9 @@ from .stlLexer import stlLexer
 from .stlParser import stlParser
 from .stl import STLAbstractSyntaxTreeExtractor
 from .stl import Operation, RelOperation, STLFormula, Trace
-from .stl2milp import stl2milp_pulp
+from .stl2milp_pulp import stl2milp_pulp
+try:
+    # will throw ImportError if gurobipy is not installed
+    from .stl2milp import stl2milp
+except ImportError:
+    pass
