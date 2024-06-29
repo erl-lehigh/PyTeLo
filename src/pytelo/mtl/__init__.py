3926d8a9e1d62558c0a058336bb51603fe7348d6
Cristian-Ioan Vasile <cvasile@lehigh.edu>
2024-06-29 01:03:30 -0400

    Corrected import in mtl subpackage, and added import of to_ast method.

--- src/pytelo/mtl/__init__.py
+++ src/pytelo/mtl/__init__.py
@@ -1,4 +1,5 @@
 from .mtlLexer import mtlLexer
 from .mtlParser import mtlParser
 from .mtl import MTLAbstractSyntaxTreeExtractor
-from .mtl import Operation, RelOperation, MTLFormula
+from .mtl import Operation, MTLFormula
+from .mtl import to_ast
--- src/pytelo/stl/__init__.py
+++ src/pytelo/stl/__init__.py
@@ -2,3 +2,4 @@ from .stlLexer import stlLexer
 from .stlParser import stlParser
 from .stl import STLAbstractSyntaxTreeExtractor
 from .stl import Operation, RelOperation, STLFormula, Trace, TraceBatch
+from .stl import to_ast
