09e89cc4ee1dda696253fa7e56fc0aff7e9d16a5
Gustavo A. Cardona <60072164+Gacardonac@users.noreply.github.com>
2024-10-29 14:26:51 -0400

    Update __init__.py

--- wmtl/__init__.py
+++ wmtl/__init__.py
@@ -1,4 +1,4 @@
 from wmtlLexer import wmtlLexer
 from wmtlParser import wmtlParser
 from wmtl import WMTLAbstractSyntaxTreeExtractor
-from mtl import Operation, RelOperation, MTLFormula
+from mtl import Operation, MTLFormula, to_ast
