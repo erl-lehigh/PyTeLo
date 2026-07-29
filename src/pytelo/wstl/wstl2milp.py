80340e9b56df49f1dfc3ff78a0720f0c1052875a
Crockett Hensley <crocketthensley@yahoo.com>
2026-07-29 16:49:00 -0400

    Fix copyright block

--- src/pytelo/wstl/wstl2milp.py
+++ src/pytelo/wstl/wstl2milp.py
@@ -1,10 +1,10 @@
 '''
  Copyright (c) 2023, Explainable Robotics Lab (ERL), Lehigh University
- See license.txt file for license information.
  @author: Gustavo A. Cardona, Cristian-Ioan Vasile
 
  Copyright (c) 2026, Explainable Robotics Lab (ERL), Lehigh University
  @editor: Crockett L. Hensley
+ See license.txt file for license information.
 '''
 
 import gurobipy as grb
