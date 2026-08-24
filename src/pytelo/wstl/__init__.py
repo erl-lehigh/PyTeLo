1b2d0a5736f6f6b0f9b88f00e4c29e153a4c2c71
Crockett Hensley <crh325@lehigh.edu>
2026-08-24 12:59:06 -0400

    Updated deployment and fixed wstl import

--- src/pytelo/wstl/__init__.py
+++ src/pytelo/wstl/__init__.py
@@ -1,4 +1,4 @@
-from wstlLexer import wstlLexer
-from wstlParser import wstlParser
+from pytelo._internal.wstlLexer import wstlLexer
+from pytelo._internal.wstlParser import wstlParser
 from wstl import WSTLAbstractSyntaxTreeExtractor, WSTLFormula, to_ast
 from stl import Operation, RelOperation, Trace
